9f934e20bd328b7f23711c062080ffb45058f9c3
Daniel Jones <messyjones@gmail.com>
2022-09-16 03:55:16 -0400

    got rid of unncessary fake data

--- debtdestroyer/SceneDelegate.swift
+++ debtdestroyer/SceneDelegate.swift
@@ -53,15 +53,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             let navController = UINavigationController(rootViewController: welcomeVC)
             set(windowScene: windowScene, startingVC: navController)
         } else {
-            let quizDataParse = QuizDataParse()
-            let topic = QuizTopicParse()
-            topic.name = "Nano"
-            topic.ticker = "XNO"
-            quizDataParse.quizTopic = topic
-            quizDataParse.correct_answer_index = 2
-            quizDataParse.answers = ["buttsajlf aslkjdfklaf sajkdfla;f lskjdf;ajsf lsdajflkajsdf;l aslkdjflksaf", "alkjdf;lafk sdajfals;jdfl aslkjdflk;asjdf saldkjflksaf lasjdfl;aj sadlfjla;k", "faskljfl;js sadjflasjdf;las ladjsflkaj asldkjfas;jf ", "asdflk askjdfkla asdkfjlsajdfk saldjfklsajflk"]
-            quizDataParse.question = "Transaction fee is zero that means if you send 1 nano counter party will receive exactly 1 nano not more or less"
-
             let vc = CryptoTabBarViewController()
             set(windowScene: windowScene, startingVC: vc)
         }

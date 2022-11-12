2f74bf1f5e608ef4d51ae70618dc3c924d08b5d7
Daniel Jones <messyjones@gmail.com>
2022-11-11 16:05:40 -0800

    added a title to the student loan accounts vc

--- debtdestroyer/Settings/Connected Accounts/ConnectedAccountsViewController.swift
+++ debtdestroyer/Settings/Connected Accounts/ConnectedAccountsViewController.swift
@@ -22,6 +22,7 @@ class ConnectedAccountsViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.title = "Your Student Loan Accounts"
         tabBarController?.tabBar.isHidden = true
         setNavBarBtns()
         loadDebtAccounts()

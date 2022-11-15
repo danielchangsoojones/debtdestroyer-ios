2b363468c1187dda06ba75476a83d5c28b81dd29
Rashmi Aher <aher604@gmail.com>
2022-11-15 14:53:44 +0530

    ResetPasswordVC Design & call to VC added

---
+++ debtdestroyer/Onboarding/Create Profile/ResetPasswordViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ResetPasswordViewController.swift
+//  debtdestroyer
+//
+//  Created by Rashmi Aher on 15/11/22.
+//
+
+import UIKit
+import SnapKit
+
+class ResetPasswordViewController: RegisterViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateLabels()
+        self.hideKeyboardWhenTappedAround()
+    }
+    
+    private func updateLabels() {
+        emailLabel.text = "Enter your email and we'll send you instructions on how to reset your password."
+        emailTextField.placeholder = "Email"
+        passwordLabel.isHidden = true
+        passwordTextField.isHidden = true
+        emailTextField.keyboardType = .emailAddress
+        
+        let logo = UIImage(named: "Login-1")
+        let imageView = UIImageView(image:logo)
+        self.navigationItem.titleView = imageView
+    }
+   
+    override func nextBtnPressed() {
+        nextButton.startSpinning()
+        if validateEmail() {
+          //emailTextField.text send to API
+            nextButton.stopSpinning()
+            
+            self.segueIntoApp()
+        } else {
+            nextButton.stopSpinning()
+        }
+    }
+        
+    override func segueIntoApp() {
+        self.navigationController?.popViewController(animated: true)
+    }
+}

44f7b4c92437e9a871a8abdf09c9cf1c364ea34d
Daniel Jones <messyjones@gmail.com>
2022-11-29 15:37:40 -0800

    the startview controller is now waiting to show the new quiz

--- debtdestroyer/Crypto/Quiz/NewGameStart/NewGameStartViewController.swift
+++ debtdestroyer/Crypto/Quiz/NewGameStart/NewGameStartViewController.swift
@@ -61,7 +61,6 @@ class NewGameStartViewController: UIViewController {
     }
     
     private func callTimer() {
-//        quizKickoffTime = Date().addingTimeInterval(timeLeft)
         checkStartTimer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(getQuizDatas), userInfo: nil, repeats: true)
         timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
     }

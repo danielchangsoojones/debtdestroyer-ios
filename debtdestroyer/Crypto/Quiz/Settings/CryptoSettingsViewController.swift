2c023b3899c02012fbd8ca9aeefd0d98dc042f98
Daniel Jones <messyjones@gmail.com>
2023-03-03 00:25:33 -0800

    the live feed works now

--- debtdestroyer/Crypto/Quiz/Settings/CryptoSettingsViewController.swift
+++ debtdestroyer/Crypto/Quiz/Settings/CryptoSettingsViewController.swift
@@ -43,12 +43,6 @@ class CryptoSettingsViewController: UIViewController {
         dataArr = ["Enter Winner Information", "Connected Accounts", "Contact Us", "Legal Disclosures", "Leave Feedback", "Notification Settings", "Log Out", "Delete Account"]
 
         imgNameArr = ["legal", "accounts", "contactUs", "legal", "feedback", "contactUs", "logout", "deleteAcc"]
-
-        //TODO: Not sure about the condition. has to work on it after logic gets clear
-//        if UserDefaults.bool("") {
-//            dataArr.insert("Promo Code", at: 2)
-//            imgNameArr.insert("legal", at: 2)
-//        }
         
         if User.current()!.email == "messyjones@gmail.com" {
             dataArr.append("Send Text Notification")

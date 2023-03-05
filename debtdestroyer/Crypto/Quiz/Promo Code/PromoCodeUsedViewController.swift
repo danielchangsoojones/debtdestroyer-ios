b5863bae5644bbb40287650ff62ee8a623d7aa42
Daniel Jones <messyjones@gmail.com>
2023-03-05 15:14:54 -0800

    got the promo made

--- debtdestroyer/Crypto/Quiz/Promo Code/PromoCodeUsedViewController.swift
+++ debtdestroyer/Crypto/Quiz/Promo Code/PromoCodeUsedViewController.swift
@@ -8,8 +8,18 @@
 import UIKit
 
 class PromoCodeUsedViewController: UIViewController {
-    private var titleLbl = UILabel()
-    private var promoInfoLabel = UILabel()
+    private var titleLbl: UILabel!
+    private var promoInfoLabel: UILabel!
+    private var promoUsers: [PromoUser] = []
+    private let dataStore = PromoDataStore()
+    
+    class PromoUser {
+        let user: User
+        
+        init(user: User) {
+            self.user = user
+        }
+    }
     
     override func loadView() {
         super.loadView()
@@ -23,5 +33,53 @@ class PromoCodeUsedViewController: UIViewController {
         super.viewDidLoad()
         self.title = "Your Promo"
         
+        
+        
+    }
+    
+    private func loadData() {
+        dataStore.getPromoInfo { promoUsers, personalPromo, promo_info_str in
+            self.promoUsers = promoUsers
+            
+            let underlineAttribute = [NSAttributedString.Key.underlineStyle: NSUnderlineStyle.thick.rawValue]
+            let underlineAttributedString = NSAttributedString(string: personalPromo, attributes: underlineAttribute)
+            self.titleLbl.attributedText = underlineAttributedString
+            
+            self.promoInfoLabel.text = promo_info_str
+        }
+    }
+}
+
+extension PromoCodeUsedViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return promoUsers.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let promoUser = promoUsers[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "basicStyle", for: indexPath)
+        cell.textLabel?.text = promoUser.user.fullName
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, estimatedHeightForHeaderInSection section: Int) -> CGFloat {
+        return 50
+    }
+    
+    internal func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 50
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerView = UIView.init(frame: CGRect.init(x: 0, y: 0, width: tableView.frame.width, height: 50))
+        
+        let label = UILabel()
+        label.frame = CGRect.init(x: 5, y: 5, width: headerView.frame.width-10, height: headerView.frame.height-10)
+        label.text = "Your Invited Friends"
+        label.font = .systemFont(ofSize: 16)
+        label.textColor = .black
+        headerView.addSubview(label)
+        
+        return headerView
     }
 }
---
+++ debtdestroyer/Crypto/Quiz/Promo Code/PromoDataStore.swift
@@ -0,0 +1,35 @@
+//
+//  PromoDataStore.swift
+//  debtdestroyer
+//
+//  Created by Daniel Jones on 3/5/23.
+//
+
+import Foundation
+import Parse
+import SwiftyJSON
+
+class PromoDataStore {
+    func getPromoInfo(completion: @escaping ([PromoCodeUsedViewController.PromoUser], String, String) -> Void) {
+        PFCloud.callFunction(inBackground: "getPromoInfo", withParameters: nil) { (results, error) in
+            if let results = results {
+                let json = JSON(results)
+                let personalPromo = json["personal_promo"].stringValue
+                let promo_info_detail = json["promo_info_detail"].stringValue
+                var users: [User] = []
+                if let dict = json.dictionaryObject, let usersFromDict = dict["users"] as? [User] {
+                    users = usersFromDict
+                }
+                let promoUsers = users.map { user in
+                    let promoUser = PromoCodeUsedViewController.PromoUser(user: user)
+                    return promoUser
+                }
+                completion(promoUsers, personalPromo, promo_info_detail)
+            } else if let error = error {
+                BannerAlert.show(with: error)
+            } else {
+                BannerAlert.showUnknownError(functionName: "getPromoInfo")
+            }
+        }
+    }
+}

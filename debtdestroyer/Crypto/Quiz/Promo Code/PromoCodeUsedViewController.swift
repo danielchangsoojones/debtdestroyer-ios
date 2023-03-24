ef347668d4a2bcf6714b7891545383ceaf2b3da9
DK <>
2023-03-24 01:43:09 -0400

    we're now able to properly load the contacts + # of overlaps. will need to show the friends who've been invited

--- debtdestroyer/Crypto/Quiz/Promo Code/PromoCodeUsedViewController.swift
+++ debtdestroyer/Crypto/Quiz/Promo Code/PromoCodeUsedViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import Contacts
+import MessageUI
 
 class PromoCodeUsedViewController: UIViewController {
     private var titleLbl: UILabel!
@@ -14,6 +16,15 @@ class PromoCodeUsedViewController: UIViewController {
     private let dataStore = PromoDataStore()
     private var tableView: UITableView!
     private var faqBtn: UIButton!
+    private var contactStore = CNContactStore()
+    private var contacts = [CNContact]()
+    private var inviteMsg = ""
+    private var messageHelper: MessageHelper?
+    private var retrievedContacts: [RetrievedContact] = []
+    private var refreshControl = UIRefreshControl()
+    private var hasAccessPermission: Bool = false
+    private var selectedContact: FriendContactParse? = nil
+    private var theSpinnerContainer: UIView!
     
     class PromoUser {
         let user: User
@@ -29,66 +40,167 @@ class PromoCodeUsedViewController: UIViewController {
         self.view = promoCodeView
         self.titleLbl = promoCodeView.titleLbl
         self.promoInfoLabel = promoCodeView.promoInfoLabel
-        self.tableView = promoCodeView.tableView
+        theSpinnerContainer = promoCodeView.theSpinnerContainer
         promoCodeView.faqBtn.addTarget(self, action: #selector(faqPressed), for: .touchUpInside)
-        promoCodeView.tableView.delegate = self
-        promoCodeView.tableView.dataSource = self
+        self.tableView = promoCodeView.tableView
+        self.tableView.addSubview(refreshControl)
+        refreshControl.addTarget(self, action: #selector(refreshData(_:)), for: .valueChanged)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(cellType: InviteContactCell.self)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Invite Friends"
-        loadData()
-        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "basicStyle")
+        messageHelper = MessageHelper(currentVC: self)
+        messageHelper?.messageDelegate = self
+        getContactAccess()
     }
     
-    private func loadData() {
-        dataStore.getPromoInfo { promoUsers, personalPromo, promo_info_str in
-            self.promoUsers = promoUsers
-            self.tableView.reloadData()
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tabBarController?.tabBar.isHidden = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.tabBarController?.tabBar.isHidden = false
+    }
+    
+    @objc func refreshData(_ sender: UIRefreshControl) {
+        loadContacts()
+        Haptics.shared.play(.heavy)
+        sender.endRefreshing()
+    }
+    
+    private func loadContacts() {
+        dataStore.loadFriendContacts(contacts: self.contacts) { promoUsers, personalPromo, promo_info_str, retrievedContacts in
+            self.theSpinnerContainer.isHidden = true
             
             let underlineAttribute = [NSAttributedString.Key.underlineStyle: NSUnderlineStyle.thick.rawValue]
             let underlineAttributedString = NSAttributedString(string: personalPromo, attributes: underlineAttribute)
             self.titleLbl.attributedText = underlineAttributedString
-            
             self.promoInfoLabel.text = promo_info_str
+            self.inviteMsg = "Yo, get this app & use my promo code \(personalPromo)! There’s a 15 question trivia every day at 6PM Pacific Time and if you get all questions correct, you win $15,000!\n\nhttps://apps.apple.com/us/app/debt-destroyed-live-trivia/id1639968618"
+            self.promoUsers = promoUsers
+            if self.hasAccessPermission {
+                self.retrievedContacts = retrievedContacts
+            }
+            self.tableView.reloadData()
         }
     }
     
-    @objc private func faqPressed() {
-        
+    private func getContactAccess() {
+        contactStore.requestAccess(for: .contacts) { (granted, error) in
+            if granted {
+                self.hasAccessPermission = granted
+                DispatchQueue.global(qos: .background).async {
+                    let keys = [CNContactGivenNameKey, CNContactFamilyNameKey, CNContactPhoneNumbersKey, CNContactThumbnailImageDataKey]
+                    let request = CNContactFetchRequest(keysToFetch: keys as [CNKeyDescriptor])
+                    do {
+                        try self.contactStore.enumerateContacts(with: request) { (contact, stop) in
+                            self.contacts.append(contact)
+                        }
+                        // Handle the fetched contacts
+                        DispatchQueue.main.async {
+                            self.loadContacts()
+                        }
+                    } catch {
+                        DispatchQueue.main.async {
+                            BannerAlert.show(title: "Error syncing contacts. Please text the Debt Destroyer team at 317-690-5323!", subtitle: error.localizedDescription, type: .error)
+                        }
+                    }
+                }
+            } else {
+                print("Contacts access denied")
+                self.hasAccessPermission = false
+                self.theSpinnerContainer.isHidden = true
+                self.loadContacts()
+                if (self.retrievedContacts.count == 0) {
+                    let alert = UIAlertController(title: "Contact Access Needed", message: "To invite friends, please update the contact access permissions in the Settings!", preferredStyle: .alert)
+                    
+                    alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+                    alert.addAction(UIAlertAction(title: "Open Settings", style: .default, handler: { _ in
+                        if let settingsUrl = URL(string: UIApplication.openSettingsURLString) {
+                            UIApplication.shared.open(settingsUrl)
+                        }
+                    }))
+                    
+                    self.present(alert, animated: true, completion: nil)
+                }
+            }
+        }
     }
+    
+    @objc private func faqPressed() {}
 }
 
 extension PromoCodeUsedViewController: UITableViewDataSource, UITableViewDelegate {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return promoUsers.count
+        return retrievedContacts.count
+//        return promoUsers.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let promoUser = promoUsers[indexPath.row]
-        let cell = tableView.dequeueReusableCell(withIdentifier: "basicStyle", for: indexPath)
-        cell.textLabel?.text = promoUser.user.fullName
+        let cell = tableView.dequeueReusableCell(for: indexPath, cellType: InviteContactCell.self)
+        let rectrievedContact = retrievedContacts[indexPath.row]
+        let contact = rectrievedContact.contact
+        let overlapCount = rectrievedContact.count
+        cell.startTextAction = {
+            let contact = rectrievedContact.contact
+            self.selectedContact = contact
+            self.messageHelper?.text(contact.phoneNumber, body: self.inviteMsg)
+        }
+        cell.set(name: contact.firstName + " " + contact.lastName, friendCount: overlapCount)
+        cell.selectionStyle = .none
         return cell
     }
     
-    func tableView(_ tableView: UITableView, estimatedHeightForHeaderInSection section: Int) -> CGFloat {
-        return 50
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableView.automaticDimension
     }
     
-    internal func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        return 50
+    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableView.automaticDimension
     }
     
-//    Don't need a header
-//    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-//        let headerView = UIView.init(frame: CGRect.init(x: 0, y: 0, width: tableView.frame.width, height: 50))
-//        let label = UILabel()
-//        label.frame = CGRect.init(x: 5, y: 5, width: headerView.frame.width-10, height: headerView.frame.height-10)
-//        label.text = "Your Invited Friends"
-//        label.font = .systemFont(ofSize: 16)
-//        label.textColor = .black
-//        headerView.addSubview(label)
-//        return headerView
-//    }
+    func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        let contentHeight = scrollView.contentSize.height
+        let scrollOffset = scrollView.contentOffset.y
+        let scrollFrameHeight = scrollView.frame.size.height
+        if (scrollOffset + scrollFrameHeight) >= contentHeight {
+            refreshControl.beginRefreshing()
+            refreshData(refreshControl)
+        }
+    }
+}
+
+extension PromoCodeUsedViewController: MFMessageComposeViewControllerDelegate {
+    func messageComposeViewController(_ controller: MFMessageComposeViewController, didFinishWith result: MessageComposeResult) {
+        switch result {
+        case .sent:
+            controller.dismiss(animated: false, completion: nil)
+            BannerAlert.show(title: "Invite Sent", subtitle: "", type: .success)
+            if let contact = selectedContact {
+                self.dataStore.setTextedFriend(textedContactObjectId: contact.objectId ?? "") {
+                    print("successfully set friend as texted")
+                }
+            }
+        case .cancelled:
+            controller.dismiss(animated: true, completion: nil)
+        case .failed:
+            controller.dismiss(animated: true, completion: nil)
+            BannerAlert.show(title: "Message Error", subtitle: "Error sending invite text", type: .error)
+        @unknown default:
+            BannerAlert.show(title: "Error", subtitle: "Please text the team at 317-690-5323, 'Error with message helper'", type: .error)
+        }
+        
+        //we need to reset the VC so that the phone #/body values are reset
+        messageHelper = MessageHelper(currentVC: self)
+    }
 }

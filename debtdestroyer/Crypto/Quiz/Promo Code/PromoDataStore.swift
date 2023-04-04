8c6bab484516bbaee845d64e202f10a92dd87ec2
DK <>
2023-04-04 19:23:44 -0400

    removed deprecated function

--- debtdestroyer/Crypto/Quiz/Promo Code/PromoDataStore.swift
+++ debtdestroyer/Crypto/Quiz/Promo Code/PromoDataStore.swift
@@ -16,34 +16,6 @@ struct RetrievedContact {
 }
 
 class PromoDataStore {
-    func getPromoInfo(completion: @escaping ([PromoCodeUsedViewController.PromoUser], String, String) -> Void) {
-        PFCloud.callFunction(inBackground: "getPromoInfo", withParameters: nil) { (results, error) in
-            if let results = results {
-                let json = JSON(results)
-                let personalPromo = json["personal_promo"].stringValue
-                let promo_info_detail = json["promo_info_detail"].stringValue
-                var users: [User] = []
-                if let dict = json.dictionaryObject, let usersFromDict = dict["users"] as? [User] {
-                    users = usersFromDict
-                }
-                let promoUsers = users.map { user in
-                    let promoUser = PromoCodeUsedViewController.PromoUser(user: user)
-                    return promoUser
-                }
-                
-                if !personalPromo.isEmpty {
-                    User.current()?.personalPromo = personalPromo
-                }
-                
-                completion(promoUsers, personalPromo, promo_info_detail)
-            } else if let error = error {
-                BannerAlert.show(with: error)
-            } else {
-                BannerAlert.showUnknownError(functionName: "getPromoInfo")
-            }
-        }
-    }
-    
     func setTextedFriend(textedContactObjectId: String, completion: @escaping () -> Void) {
         let parameters: [String: Any] = ["textedContactObjectId": textedContactObjectId]
         PFCloud.callFunction(inBackground: "setTextedFriend", withParameters: parameters) { (result, error) in

738063da2c00e4c45f2fc1824194492e03bf7215
DK <>
2023-04-15 16:13:56 -0400

    updated notion vc

--- debtdestroyer/Crypto/Quiz/Settings/Notion/NotionViewController.swift
+++ debtdestroyer/Crypto/Quiz/Settings/Notion/NotionViewController.swift
@@ -8,74 +8,49 @@
 import UIKit
 import WebKit
 
-class NotionViewController: UIViewController, WKNavigationDelegate, WKScriptMessageHandler {
-    private var webView: WKWebView!
-    // the handler name injected into the web app for iOS webviews.
-    let vouchedHandler = "onVouchedVerify"
+class NotionViewController: UIViewController {
+    private let webView: WKWebView = {
+        let webView = WKWebView()
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        return webView
+    }()
+    
+    private var theSpinnerContainer: UIView!
     let appUrl: String!
     
-    init(notionURL: String!) {
+    init(notionURL: String) {
         self.appUrl = notionURL
         super.init(nibName: nil, bundle: nil)
     }
     
-    required init?(coder: NSCoder) {
+    required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.tabBarController?.tabBar.isHidden = true
-        
-        DispatchQueue.main.async {
-            if let url = URL(string: self.appUrl) {
-                self.webView.load(URLRequest(url: url))
-            }
+        view.backgroundColor = .white
+        view.addSubview(webView)
+        theSpinnerContainer = Helpers.showActivityIndicatory(in: webView)
+        view.addSubview(theSpinnerContainer)
+        webView.snp.makeConstraints { make in
+            make.leading.trailing.bottom.equalToSuperview()
+            make.top.equalTo(self.view.snp.topMargin)
+        }
+        theSpinnerContainer.snp.makeConstraints { make in
+            make.center.equalToSuperview()
         }
         
-        self.navigationItem.title = ""
-        self.navigationController?.navigationBar.barTintColor = .white
-        self.navigationController?.navigationBar.backgroundColor = .white
-        self.navigationController?.navigationBar.tintColor = .black
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        tabBarController?.tabBar.isHidden = true
-    }
-    
-    override func loadView() {
-        super.loadView()
-        let notionView = NotionView(frame: self.view.bounds)
-        self.view = notionView
-        webView = notionView.webView
+        if let url = URL(string: appUrl) {
+            webView.load(URLRequest(url: url))
+        }
         
-        let config = WKWebViewConfiguration()
-        config.userContentController.add(self, name: vouchedHandler)
-        config.allowsInlineMediaPlayback = true
         webView.navigationDelegate = self
     }
-    
-    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
-        if message.name == vouchedHandler {
-            guard let verifyData =
-                    convertResponseToDict(payload: message.body as! String),
-                  let results = verifyData["result"] as? [String: AnyObject],
-                  let successCode = results["success"] as? Bool  else {
-                return
-            }
-        }
-    }
-    
-    func convertResponseToDict(payload: String) -> [String:AnyObject]? {
-        if let data = payload.data(using: .utf8) {
-            do {
-                let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String:AnyObject]
-                return json
-            } catch {
-                print("Unable to convert verification response")
-            }
-        }
-        return nil
+}
+
+extension NotionViewController: WKNavigationDelegate {
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        theSpinnerContainer.isHidden = true
     }
 }
